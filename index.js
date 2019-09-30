8f03c0d0560555daa6d64a400f7480a8824c9bbc
Taran <thetaranneeld@gmail.com>
2019-09-30 15:38:16 -0600

    get and post requests working

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+server.post('/api/users', (req, res) => {
+    const userData = req.body;
+    if (!userData) {
+        res.status(400).json({ message: 'name not found' });
+    } else {
+        db.insert(userData)
+            .then(user => {
+                res.json(user);
+            })
+            .catch(err => {
+                res.json({ message: 'user must have a name and bio' });
+            });
+    }
+});
+
+const port = 5000;
+server.listen(port, () => console.log('\nserver running\n'));
